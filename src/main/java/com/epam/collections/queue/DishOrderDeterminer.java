8cc4772960aee504eba664748f4dff607da98639
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-06-25 23:30:16 +0300

    Queue Interface Tasks solution

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,31 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.offer(i);
+        }
+
+        int dishesEaten = 0;
+        while (!queue.isEmpty()) {
+            int dish = queue.poll();
+            dishesEaten++;
+
+            if (dishesEaten % everyDishNumberToEat == 0) {
+                result.add(dish);
+            } else {
+                queue.offer(dish);
+            }
+        }
+
+        return result;
     }
 }
